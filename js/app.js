4195d6ae22eaa88c9e166a6c919d5d0f87cd81fc
marcuspereiragithub <vinnycoast@gmail.com>
2018-03-16 17:15:05 -0300

    Adding class to the click optimized

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('[data-group]').each(function(){
+        var $allTarget = $(this).find('[data-target]'),
+            $allClick = $(this).find('[data-click]'),
+            activeClass = 'active';
+
+        $allTarget.first().addClass(activeClass);
+        $allClick.first().addClass(activeClass);
+
+        $allClick.click(function(event){
+            event.preventDefault();
+            var id = $(this).data('click'),
+                $target = $('[data-target="' + id + '"]');
+
+            $allClick.removeClass(activeClass);
+            $allTarget.removeClass(activeClass);
+
+            $target.addClass(activeClass);
+            $(this).addClass(activeClass);
+        });
+    });
+});
